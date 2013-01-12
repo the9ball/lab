78b13dc10282cb0abb8c5d2a2637ad3a70fe311c
Shaula <the9ball@gmail.com>
2013-01-12 18:59:37 +0900

    テスト作成テスト

--- src/a.cpp
+++ src/a.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <PCUnit.h>
 #include "a.h"
 
 A::A()
@@ -11,3 +12,22 @@ A::~A()
 	printf( "Bye\n" );
 }
 
+int add( int a, int b ){ return a + b; }
+
+static void test_add( void )
+{
+	PCU_ASSERT_EQUAL( 3, add( 2, 1 ) );
+	PCU_ASSERT_EQUAL( 9, add( 6, 3 ) );
+	PCU_ASSERT_EQUAL( 7, add( 4, 3 ) );
+	PCU_ASSERT_EQUAL( 8, add( 2, 6 ) );
+}
+
+PCU_Suite* AddTest_suite( void )
+{
+	static PCU_Test tests[] =
+	{
+		{ "test_add", test_add },
+	};
+	static PCU_Suite suite = { "AddTest", tests, sizeof( tests ) / sizeof( tests[0] ) };
+	return &suite;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <PCUnit.h>
 
 #include "a.h"
 
+extern PCU_Suite* AddTest_suite( void );
+
+void test( void )
+{
+	const PCU_SuiteMethod suites[] =
+	{
+		AddTest_suite,
+	};
+	PCU_set_putchar( putchar );
+	PCU_run( suites, sizeof( suites ) / sizeof( suites[0] ) );
+}
+
 int main()
 {
-	A a;
-	printf( "Hello World\n" );
+	test();
 	return 0;
 }
 
