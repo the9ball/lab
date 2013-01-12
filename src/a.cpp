60f4868d4970310947f9d80189626569f18c50a5
Shaula <the9ball@gmail.com>
2013-01-12 11:55:31 +0900

    テスト用クラス作成

---
+++ src/a.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "a.h"
+
+A::A()
+{
+	printf( "Hello\n" );
+}
+
+A::~A()
+{
+	printf( "Bye\n" );
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+#include "a.h"
+
 int main()
 {
+	A a;
 	printf( "Hello World\n" );
 	return 0;
 }
