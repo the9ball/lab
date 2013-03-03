fc2b76e2dbb9acaea25bfe98eca06655a6792d3c
Shaula <the9ball@gmail.com>
2013-03-04 02:08:30 +0900

    なんだかよくわからないけどなんかファイルがあったのでコミットしておく

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <string>
+
+void p( std::string s )
+{
+	printf( "%s\n", s.c_str() );
+}
 
 int main()
 {
-	printf( "Hello World\n" );
+	p( "Hello World\n" );
 	return 0;
 }
 
